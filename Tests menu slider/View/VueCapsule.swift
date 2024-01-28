5175961c00928f8404aede24fd4571960d9b7995
Philippe MICHEL <philippe.michel1@gmail.com>
2024-01-28 23:30:35 +0100

    creation de la vue capsule plus modification des  autres vues

---
+++ Tests menu slider/View/VueCapsule.swift
@@ -0,0 +1,18 @@
+//
+//  VueCapsule.swift
+//  Menu Slider
+//
+//  Created by Philippe MICHEL on 28/01/2024.
+//
+
+import SwiftUI
+
+struct VueCapsule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VueCapsule()
+}
